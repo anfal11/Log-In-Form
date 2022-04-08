dcf5745a538b1adb4daa1d0b49371f70f3260d34
ratul <anfal15-14303@diu.edu.bd>
2022-04-08 06:51:14 +0600

    init

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Success");
+function validate(params) {
+    let username=document.getElementById("username").value;
+    let password=document.getElementById("password").value;
+}
+
+if (username=="admin"&& password=="user") {
+    alert("Log in successful");
+    
+} else {
+    alert("Log in fail");
+}
